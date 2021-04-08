3c1703b3f70147730a5383856972344b1b2f0d12
SamSmith <sam@gsamsmith.com>
2021-04-07 14:16:54 -1000

    inital commit

---
+++ wpe-page-template.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wpe Page Template
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wpe-page-template
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wpe_Page_Template
+ */
+
+// Your code starts here.
